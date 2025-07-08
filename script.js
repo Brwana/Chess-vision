c1b030424e5f6aed3516014c40c99fea9300e102
brwan <brwana110@gmail.com>
2025-07-09 02:57:20 +0300

    colored grid and output displayed for one second

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 
 const container=document.createElement('div');
 
-for (i='a'.charCodeAt(0);i<'i'.charCodeAt(0);i++){
+for (i='h'.charCodeAt(0);i>='a'.charCodeAt(0);i--){
+
     for(j=0;j<8;j++){
+
     const button=document.createElement('button');
+    if(i%2==0 && j%2==0 ||i%2!=0 && j%2!=0 ){
+
+       button.id="similar";
+    }
+    else{
+        button.id="not_similar";
+    }
+
     button.textContent=`${String.fromCharCode(i)}${j}`;
+
+    const content=document.getElementById("button_content");
+
+    button.onclick=function (){
+
+    content.textContent=button.textContent;
+    content.style.display="block";
+
+    setTimeout(() => {
+        content.style.display="none";
+    }, 1000);
+};
     container.appendChild(button);
     }
 container.appendChild(document.createElement('br'));
 
 }
-document.getElementById("grid").appendChild(container)
+document.getElementById("grid").appendChild(container);
+
+
